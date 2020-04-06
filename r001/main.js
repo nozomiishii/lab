bcc4c671ae8e637193d66f0d41e5b35597e6e177
Nozomi Ishii <non.ankt@gmail.com>
2020-04-07 04:32:09 +0900

    day01

--- r001/main.js
+++ r001/main.js
@@ -1,3 +1,24 @@
+const { Button, createMuiTheme, makeStyles, ThemeProvider } = MaterialUI;
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#4BD6E1",
+    },
+    secondary: {
+      main: "#FFD84C",
+    },
+  },
+});
+
+const useStyle = makeStyles((theme) => ({
+  dinosaur: {
+    width: "100vw",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
+
 const App = () => {
   const [speech, setSpeech] = React.useState(false);
 
@@ -8,11 +29,21 @@ const App = () => {
     setSpeech(!speech);
   };
   return (
-    <div onClick={handleClick}>
-      🦕{"{"}
-      {speech ? comment : "click me"})
+    <div>
+      <div>
+        🦕{"{"}
+        {speech ? comment : "click me"})
+      </div>
+      <Button variant="contained" onClick={handleClick}>
+        Hey!
+      </Button>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("app")
+);
