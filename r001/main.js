92d9e425383b3d9e423a575a54ce4845270f683f
Nozomi Ishii <non.ankt@gmail.com>
2020-04-07 04:49:13 +0900

    day1

--- r001/main.js
+++ r001/main.js
@@ -12,29 +12,38 @@ const theme = createMuiTheme({
 });
 
 const useStyle = makeStyles((theme) => ({
-  dinosaur: {
+  main: {
     width: "100vw",
     display: "flex",
+    flexDirection: "column",
     alignItems: "center",
+    justifyContent: "center",
+    border: "solid 1px red",
+    height: "100vh",
+  },
+  dinosaur: {
+    fontSize: "4rem",
   },
 }));
 
 const App = () => {
   const [speech, setSpeech] = React.useState(false);
 
-  const comment = "ya man! React Lab Start!!!";
+  const comment = "ya man! let's get started React Lab!!!";
   const handleClick = () => {
     console.log("click");
     console.log(speech);
     setSpeech(!speech);
   };
+
+  const classes = useStyle();
   return (
-    <div>
-      <div>
+    <div className={classes.main}>
+      <div className={classes.dinosaur}>
         🦕{"{"}
-        {speech ? comment : "click me"})
+        {speech ? comment : "..."})
       </div>
-      <Button variant="contained" onClick={handleClick}>
+      <Button variant="contained" color="primary" onClick={handleClick}>
         Hey!
       </Button>
     </div>
