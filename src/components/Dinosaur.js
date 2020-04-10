546a55c10d2e72dbdfbc125074a85c0b9ba7c864
Nozomi Ishii <non.ankt@gmail.com>
2020-04-10 17:38:37 +0900

    day 001

---
+++ src/components/Dinosaur.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Dinosaur = () => (
+  <span role="img" aria-label="dinosaur">
+    🦕
+  </span>
+);
+
+export default Dinosaur;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-const R = 5;
-const App = () => {
-  const pages = [];
-  let num;
-  for (let i = 1; i <= R; i++) {
-    if (i < 10) {
-      num = `00${String(i)}`;
-    } else if (i < 100) {
-      num = `0${String(i)}`;
-    }
-    pages.push(
-      <div key={`/r${num}`}>
-        <a href={`/r${num}`}>{i}</a>
-      </div>
-    );
-  }
-  return (
-    <div>
-      <h1>lab lists will be rendered here</h1>
-      {pages}
-    </div>
-  );
-};
+import R000 from "./r000/main";
+import R001 from "./r001/main";
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={R000} />
+      <Route exact path="/r001" component={R001} />
+    </Switch>
+  </BrowserRouter>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
