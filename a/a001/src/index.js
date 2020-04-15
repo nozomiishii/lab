5c479a04a65ccedf78c03677f1ce14e9492064d8
Nozomi Ishii <non.ankt@gmail.com>
2020-04-15 21:35:26 +0900

    accessing is work

--- a/a001/src/index.js
+++ a/a001/src/index.js
@@ -1,15 +1,28 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const forcast = require("./utils/forcast");
 
-const publicPath = path.join(__dirname, "../public");
+// const publicPath = path.join(__dirname, "../public");
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("working");
+app.get("", (req, res) => {
+  console.log("work");
+  forcast((error, response) => {
+    if (error) {
+      return res.send({
+        error: "address is invaild",
+      });
+    }
+    res.send({
+      name: req.query.address,
+      temperture: response,
+    });
+  });
+  // res.send("hey");
 });
 
 app.listen(port, () => console.log(`listening${port}`));
---
+++ a/a001/src/utils/forcast.js
@@ -0,0 +1,25 @@
+const request = require("postman-request");
+
+const forcast = (callback) => {
+  const api = "http://api.weatherstack.com/current";
+  const access_key = "?access_key=4534b4c0d66c943670b34e593e5a0d90";
+  // const query = `&query=${encodeURIComponent(latitude)},${encodeURIComponent(
+  //   longitude
+  // )}`;
+  const query = `&query=Tokyo`;
+  const url = api + access_key + query;
+  console.log(url);
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("unable to connect to location service", undefined);
+    } else if (body.error) {
+      callback("unable to find location. try another search.", undefined);
+    } else {
+      const { temperature } = body.current;
+      callback(undefined, temperature);
+    }
+  });
+};
+
+module.exports = forcast;
