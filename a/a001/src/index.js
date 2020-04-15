bc513104c00f4abe6d547f2f69f9eadffa77dc77
Nozomi Ishii <non.ankt@gmail.com>
2020-04-15 21:19:50 +0900

    add postman request for dependencis

--- a/a001/src/index.js
+++ a/a001/src/index.js
@@ -1,9 +1,14 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
+const publicPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicPath));
+
 const port = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.send("working");
 });
 
