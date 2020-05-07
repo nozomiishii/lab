6afeee3d92b61b5ff581fe7053dfccc9494e8cc3
Nozomi Ishii <non.ankt@gmail.com>
2020-05-07 19:23:55 +0900

    typescript

--- t/index.js
+++
@@ -1,3 +0,0 @@
-const string = "1";
-console.log(Number(string));
-console.log(+string); //上と同じ
--- t/index.ts
+++ t/index.ts
@@ -1,1 +1,9 @@
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2")! as HTMLInputElement;
+const btn = document.getElementById("button");
 
+const addNum = (a: number, b: number) => {
+  console.log(a + b);
+};
+
+btn.addEventListener("click", () => addNum(+input1.value, +input2.value));
