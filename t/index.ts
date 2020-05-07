438b29fc1d6dd36559e3eed712ba91917b842542
Nozomi Ishii <non.ankt@gmail.com>
2020-05-08 00:35:15 +0900

    ts

--- t/index.ts
+++ t/index.ts
@@ -1,4 +1,4 @@
-const input1 = document.getElementById("input1");
+const input1 = document.getElementById("input1")! as HTMLInputElement;
 const input2 = document.getElementById("input2")! as HTMLInputElement;
 const btn = document.getElementById("button");
 
