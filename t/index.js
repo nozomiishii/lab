0664044ba333b87c9f1f14d6ec81be6dcc7cf6af
Nozomi Ishii <non.ankt@gmail.com>
2020-05-08 14:25:30 +0900

    ts

--- t/index.js
+++ t/index.js
@@ -1,18 +1,13 @@
 // const input1 = document.getElementById("input1")! as HTMLInputElement;
 // const input2 = document.getElementById("input2")! as HTMLInputElement;
 // const btn = document.getElementById("button");
-
 // const addNum = (a: number, b: number) => {
 //   console.log(a + b);
 // };
-
 // btn.addEventListener("click", () => addNum(+input1.value, +input2.value));
-
-const add = (a, b) => {
-  return a + b;
+var add = function (a, b) {
+    return a + b;
 };
-
-const number1 = 5099;
-const number2 = 5820384952;
-
+var number1 = 5099;
+var number2 = 5820384952;
 console.log(add(number1, number2));
