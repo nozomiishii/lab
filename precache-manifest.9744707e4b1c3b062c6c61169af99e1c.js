8c5fb9293694f9725fafab9f3f9bfa21065736dc
Nozomi Ishii <non.ankt@gmail.com>
2020-05-25 22:51:28 +0900

    Updates

---
+++ precache-manifest.9744707e4b1c3b062c6c61169af99e1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b331d13bbc017319b13e60908eb24c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "949baa8ded4ec19fe664",
+    "url": "/static/js/2.e6198540.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e6198540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068c803c62554a4b2018",
+    "url": "/static/js/main.64e0f033.chunk.js"
+  },
+  {
+    "revision": "3f07ac288aad444fa7fe",
+    "url": "/static/js/runtime-main.ad2a1d03.js"
+  }
+]);
